daf0ccc448eab5f2e1245210d938a22dcefbbfe5
psCommune <daniil.mukhametov.96@mail.ru>
2025-10-20 10:32:11 +0500

    added layout

--- EmployeeSalaryProcessor/AddEmployeeWindow.xaml.cs
+++ EmployeeSalaryProcessor/AddEmployeeWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,21 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace EmployeeSalaryProcessor
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Логика взаимодействия для AddEmployeeWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class AddEmployeeWindow : Window
     {
-        public MainWindow()
+        public AddEmployeeWindow()
         {
             InitializeComponent();
         }
-
-
     }
 }
